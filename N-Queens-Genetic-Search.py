58ea435c2fbbb94d4c4fcfa60a718f94e2aa121d
t-smaalumi <t-smaalumi@microsoft.com>
2024-08-29 12:46:41 +0300

    genetic algorithem

--- N-Queens-Genetic-Search.py
+++ N-Queens-Genetic-Search.py
@@ -1,168 +1,101 @@
-# Mahdi Hassanzadeh
-from operator import indexOf
 import random
 
 
-# Making random chromosomes
-def random_chromosome(size):
-    return [random.randint(0, size - 1) for _ in range(size)]
+class GeneticAlgorithmNQueens:
+    """
+    Genetic Algorithm for solving the N-Queens problem.
 
+    Steps:
+    1. Initialize a random population of solutions.
+    2. Evaluate fitness of each individual.
+    3. Select parents based on fitness.
+    4. Perform crossover and mutation to produce offspring.
+    5. Repeat the process until a solution is found or termination criteria are met.
 
-# Calculating fitness
-def fitness(chromosome, maxFitness):
-    # [0, 1, 2, 3, 4, 5, 6, 7]
+    The running time is O(t * n^2) where t is the number of generations and n is the size of the board.
+    """
 
-    horizontal_collisions = (sum([chromosome.count(queen) - 1 for queen in chromosome]) / 2)
-    diagonal_collisions = 0
+    def __init__(self, n, population_size=100, mutation_rate=0.05, max_generations=1000):
+        self.n = n
+        self.population_size = population_size
+        self.mutation_rate = mutation_rate
+        self.max_generations = max_generations
+        self.population = self.init_population()
+        self.best_solution = None
 
-    n = len(chromosome)
-    left_diagonal = [0] * (2 * n - 1)
-    right_diagonal = [0] * (2 * n - 1)
+    def init_population(self):
+        """Initialize a random population of individuals."""
+        return [self.random_board() for _ in range(self.population_size)]
 
-    for i in range(n):
-        left_diagonal[i + chromosome[i] - 1] += 1
-        right_diagonal[len(chromosome) - i + chromosome[i] - 2] += 1
+    def random_board(self):
+        """Generate a random board configuration."""
+        return [random.randint(0, self.n - 1) for _ in range(self.n)]
 
-    diagonal_collisions = 0
-    for i in range(2 * n - 1):
-        counter = 0
-        if left_diagonal[i] > 1:
-            counter += left_diagonal[i] - 1
-        if right_diagonal[i] > 1:
-            counter += right_diagonal[i] - 1
-        diagonal_collisions += counter
+    def fitness(self, board):
+        """Calculate fitness based on the number of non-conflicting pairs."""
+        max_pairs = self.n * (self.n - 1) // 2
+        conflicts = 0
+        for i in range(self.n):
+            for j in range(i + 1, self.n):
+                if board[i] == board[j] or abs(board[i] - board[j]) == abs(i - j):
+                    conflicts += 1
+        return max_pairs - conflicts
 
-    # 28-(2+3)=23
-    return int(maxFitness - (horizontal_collisions + diagonal_collisions))
+    def select_parents(self):
+        """Select two parents using tournament selection."""
+        tournament_size = 5
+        return [max(random.sample(self.population, tournament_size), key=self.fitness) for _ in range(2)]
 
+    def crossover(self, parent1, parent2):
+        """Perform crossover between two parents to create an offspring."""
+        crossover_point = random.randint(0, self.n - 1)
+        child = parent1[:crossover_point] + parent2[crossover_point:]
+        return child
 
-# Doing cross_over between two chromosomes
-def crossover(x, y):
-    n = len(x)
-    child = [0] * n
-    for i in range(n):
-        c = random.randint(0, 1)
-        if c < 0.5:
-            child[i] = x[i]
+    def mutate(self, board):
+        """Mutate the board by randomly changing the position of one queen."""
+        if random.random() < self.mutation_rate:
+            board[random.randint(0, self.n - 1)] = random.randint(0, self.n - 1)
+        return board
+
+    def evolve(self):
+        """Run the Genetic Algorithm to evolve the population."""
+        for generation in range(self.max_generations):
+            new_population = []
+            for _ in range(self.population_size):
+                parent1, parent2 = self.select_parents()
+                child = self.crossover(parent1, parent2)
+                child = self.mutate(child)
+                new_population.append(child)
+
+            self.population = new_population
+            best_board = max(self.population, key=self.fitness)
+            if self.fitness(best_board) == self.n * (self.n - 1) // 2:
+                self.best_solution = best_board
+                break
+
+        if not self.best_solution:
+            self.best_solution = max(self.population, key=self.fitness)
+
+    def print_solution(self):
+        """Print the best solution found."""
+        if self.best_solution:
+            board = self.best_solution
+            for i in range(self.n):
+                row = ['Q' if board[j] == i else '.' for j in range(self.n)]
+                print(' '.join(row))
+            print("Number of conflicts:", self.n * (self.n - 1) // 2 - self.fitness(self.best_solution))
         else:
-            child[i] = y[i]
-    return child
+            print("No solution found.")
 
 
-# Randomly changing the value of a random index of a chromosome
-def mutate(x):
-    n = len(x)
-    c = random.randint(0, n - 1)
-    m = random.randint(0, n - 1)
-    x[c] = m
-    return x
+if __name__ == '__main__':
+    n = 8
+    total_steps = 0
+    for _ in range(10):
+        genetic_algorithm = GeneticAlgorithmNQueens(n)
+        genetic_algorithm.evolve()
+        genetic_algorithm.print_solution()
+        print()
 
-
-# Calculating probability
-def probability(chromosome, maxFitness):
-    return fitness(chromosome, maxFitness) / maxFitness
-
-
-# Roulette-wheel selection
-def random_pick(population, probabilities):
-    populationWithProbabilty = zip(population, probabilities)
-    total = sum(w for c, w in populationWithProbabilty)
-    r = random.uniform(0, total)
-    upto = 0
-    for c, w in zip(population, probabilities):
-        if upto + w >= r:
-            return c
-        upto += w
-    assert False, "Shouldn't get here"
-
-
-# Genetic algorithm
-def genetic_queen(population, maxFitness):
-    mutation_probability = 0.1
-    new_population = []
-    sorted_population = []
-    probabilities = []
-    for n in population:
-        f = fitness(n, maxFitness)
-        probabilities.append(f / maxFitness)
-        sorted_population.append([f, n])
-
-    sorted_population.sort(reverse=True)
-
-    # Elitism
-    new_population.append(sorted_population[0][1])  # the best gen
-    new_population.append(sorted_population[-1][1])  # the worst gen
-
-    for i in range(len(population) - 2):
-
-        chromosome_1 = random_pick(population, probabilities)
-        chromosome_2 = random_pick(population, probabilities)
-
-        # Creating two new chromosomes from 2 chromosomes
-        child = crossover(chromosome_1, chromosome_2)
-
-        # Mutation
-        if random.random() < mutation_probability:
-            child = mutate(child)
-
-        new_population.append(child)
-        if fitness(child, maxFitness) == maxFitness:
-            break
-    return new_population
-
-
-# prints given chromosome
-def print_chromosome(chrom, maxFitness):
-    print(
-        "Chromosome = {},  Fitness = {}".format(str(chrom), fitness(chrom, maxFitness))
-    )
-
-
-# prints given chromosome board
-def print_board(chrom):
-    board = []
-
-    for x in range(nq):
-        board.append(["x"] * nq)
-
-    for i in range(nq):
-        board[chrom[i]][i] = "Q"
-
-    def print_board(board):
-        for row in board:
-            print(" ".join(row))
-
-    print()
-    print_board(board)
-
-
-if __name__ == "__main__":
-    POPULATION_SIZE = 500
-    nq = 8
-
-
-    maxFitness = (nq * (nq - 1)) / 2  # represents the maximum number of non-attacking pairs of queens
-    population = [random_chromosome(nq) for _ in range(POPULATION_SIZE)] # initial population of chromosomes
-
-    # example of population: [[0, 1, 2, 3, 4, 5, 6, 7], [7, 6, 5, 4, ... ], ...]
-
-    generation = 1
-    while maxFitness not in [fitness(chrom, maxFitness) for chrom in population] and generation < 200:
-        population = genetic_queen(population, maxFitness)
-        if generation % 10 == 0:
-            print("=== Generation {} ===".format(generation))
-            print("Maximum Fitness = {}".format(max([fitness(n, maxFitness) for n in population])))
-        generation += 1
-
-    fitnessOfChromosomes = [fitness(chrom, maxFitness) for chrom in population]
-
-    bestChromosomes = population[indexOf(fitnessOfChromosomes, max(fitnessOfChromosomes))]
-
-    if maxFitness in fitnessOfChromosomes:
-        print("\nSolved in Generation {}!".format(generation - 1))
-        print_chromosome(bestChromosomes, maxFitness)
-        print_board(bestChromosomes)
-
-    else:
-        print("\nUnfortunately, we could't find the answer until generation {}. The best answer that the algorithm found was:".format(generation - 1))
-        print_board(bestChromosomes)
+    print(f"Average number of generations: {total_steps / 10}")
